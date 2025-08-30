8636cb567fd67c9242e014e1a434c54e98a2f257
白登超 <bacium_dc@163.com>
2025-08-30 23:15:56 +0800

    fix:upgrade upload file

--- javaBase/src/test/socket/uploadFile/ReciverFile.java
+++ javaBase/src/test/socket/uploadFile/ReciverFile.java
@@ -3,6 +3,7 @@ package test.socket.uploadFile;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.UUID;
 
 /**
  * @Author: bai12
@@ -13,28 +14,32 @@ import java.net.Socket;
 public class ReciverFile {
     public static void main (String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(9999);
+
         System.out.println("Waiting for a client ...");
-        Socket socket = serverSocket.accept();
-        BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
+        while (true) {
+            Socket socket = serverSocket.accept();
+            BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
+            UUID uuid = UUID.fromString(UUID.randomUUID().toString());
 
-        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("javaBase\\src\\test\\socket\\uploadFile\\file\\copyAvatar.png"));
-        int b;
-        while ((b = bis.read()) != -1) {
-            bos.write(b);
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("javaBase\\src\\test\\socket\\uploadFile\\file\\"+ uuid +".png"));
+            int b;
+            while ((b = bis.read()) != -1) {
+                bos.write(b);
+            }
+
+            InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+            BufferedReader br = new BufferedReader(isr);
+
+            OutputStreamWriter osw = new OutputStreamWriter(socket.getOutputStream());
+            BufferedWriter bw = new BufferedWriter(osw);
+            bw.write("上传成功");
+            bw.newLine();
+            bw.flush();
+
+            bw.close();
+            br.close();
+            socket.close();
         }
-
-        InputStreamReader isr = new InputStreamReader(socket.getInputStream());
-        BufferedReader br = new BufferedReader(isr);
-
-        OutputStreamWriter osw = new OutputStreamWriter(socket.getOutputStream());
-        BufferedWriter bw = new BufferedWriter(osw);
-        bw.write("上传成功");
-        bw.newLine();
-        bw.flush();
-
-        bw.close();
-        br.close();
-        socket.close();
-        serverSocket.close();
+        // serverSocket.close();
     }
 }
